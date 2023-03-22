e6b5f411a574de967a794175a0f320ba59990fb8
Gustavosl1 <gusouzalima5@gmail.com>
2023-03-22 15:33:53 -0300

    commit navbar corrigido

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
+const btnMobile = document.getElementById('btn-mobile');
 
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
 
-hamburguer.addEventListener("click",() => {
-    hamburguer.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
